3360e62541da6d2743014fb4d0e5aea0ead783f5
Tvbuddies <64229309+Tvbuddies@users.noreply.github.com>
2021-06-21 12:21:20 +0400

    Game source code
    In here you can download the source code and commit some code/edits
    
    the game is still not complete yet so if you want to commit to the game/clean some code or add online multiplayer feel free!

---
+++ Teamshootout/Players/Scripts/player3tscn.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+var MAX_SPEED = 1000
+var ACCELERATION = 3000
+var motion = Vector2.ZERO
+
+func _physics_process(delta):
+	var axis = get_axis()
+	if axis == Vector2.ZERO:
+		apply_friction(ACCELERATION * delta)
+	else:
+		apply_movement(axis * ACCELERATION * delta)
+	motion = move_and_slide(motion)
+
+func get_axis():
+	var axis = Vector2.ZERO
+	axis.x = int(Input.is_action_pressed("p3r")) - int(Input.is_action_pressed("p3l"))
+	axis.y = int(Input.is_action_pressed("p3d")) - int(Input.is_action_pressed("p3u"))
+	return axis.normalized()
+
+func apply_friction(amount):
+	if motion.length() > amount:
+		motion -= motion.normalized() * amount
+	else:
+		motion = Vector2.ZERO
+
+func apply_movement(acceleration):
+	motion += acceleration
+	motion = motion.clamped(MAX_SPEED)
