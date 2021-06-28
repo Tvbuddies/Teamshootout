c598a0342640a7cbd1c15c3147500f8902796f35
Planet junkie <64229309+Tvbuddies@users.noreply.github.com>
2021-06-28 18:02:08 +0400

    game update

--- Teamshootout/Sandbox/sandbox.gd
+++ Teamshootout/Sandbox/sandbox.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+var enemy_1 = preload("res://enemy/enemy.tscn")
 
-func ready():
-	if Input.is_action_pressed("slowmo"):
-		$ColorRect.show()
-	else:
-		$ColorRect.hide()
+
+func _on_enemyspawntimer_timeout() -> void:
+	var enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	
+	while enemy_position.x < 640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
+		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	
+	Global.instance_node(enemy_1, enemy_position, self)
---
+++ Teamshootout/mainmenu.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_Button_pressed() -> void:
+	get_tree().change_scene("res://Sandbox/sandbox.tscn")
